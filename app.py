3cbf551424be5e719a4139a0b98a609718e1f30c
MattTom <mattjohans688@gmail.com>
2021-11-01 16:14:46 +0400

    Use these as attached file in Python

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Hi What is your name?")
+name = input()
+print("Hello " + name)
+print(" We are going to take a small quiz based on Avengers today...")
+print(" Can we start??")
+Answer = input()
+if Answer := 'Yes':
+    print(" Great")
+print(" #Type your questions here ")
